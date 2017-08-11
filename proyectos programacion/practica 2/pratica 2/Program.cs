f4b2eb8f7a2ca639a7c45cb1458de0f5971bf73d
alfredovazquezdiaz <https://github.com/alfredovazquezdiaz/proyectos.git>
2017-08-11 12:17:24 -0400

    proyectos

---
+++ proyectos programacion/practica 2/pratica 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_2
+{
+    class PruebaVector
+    { 
+        private string[] nombres; // declaramos un vector de tipo estring para los nombres
+        private float[] sueldos; //declaramos un vector de tipo float para los sueldos
+        public void Cargar()
+        {
+            nombres = new string[5];
+            sueldos = new float[5];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+
+                Console.Write("Ingrese el nombre del empleado " + (f + 1) + ": ");
+                nombres[f] = Console.ReadLine();
+                Console.Write("Ingrese el sueldo " + (f + 1) + ": ");
+                string linea;
+                linea = Console.ReadLine();
+                sueldos[f] = float.Parse(linea);
+            }
+        }
+        public void MayorSueldo()
+        {
+            float mayor;
+            int pos;
+            mayor = sueldos[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (sueldos[f] > mayor)
+                {
+                    mayor = sueldos[f];
+                    pos = f;
+
+                }
+            }
+            Console.WriteLine("El empleado con sueldo mayor es :" + nombres[pos]);
+            Console.WriteLine("tiene un sueldo de :" + mayor);
+        }
+        static void Main(string[] args)
+        {
+            PruebaVector pv = new PruebaVector();
+
+            Console.WriteLine("Nombre:Oscar Carmona Mendez 16-SIIN-1-164 seccion: 0463");
+
+            Console.WriteLine("Nombre:Alfredo Vasquez Diaz 15-MIIN-1-138 seccion: 0463");
+
+            pv.Cargar();
+            pv.MayorSueldo();
+        }
+    }
+}
---
+++ proyectos programacion/practica 3/practica 3/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+namespace practica_3
+
+
+{
+    class Cliente
+
+    {
+        private string nombre;
+
+        private int monto;
+
+        public Cliente(string nom)
+
+        {
+
+            nombre = nom;
+
+            monto = 0;
+
+        }
+
+        public void Depositar(int m)
+
+        {
+
+            monto = monto + m;
+
+        }
+
+        public void Extraer(int m)
+
+        {
+
+            monto = monto - m;
+
+        }
+
+        public int RetornarMonto()
+
+        {
+
+            return monto;
+        }
+
+        public void Imprimir()
+
+        {
+
+            Console.WriteLine(nombre + "tiene depositado la suma de " + monto);
+
+        }
+    }
+
+    class Banco
+
+    {
+
+        private Cliente cliente1, cliente2, cliente3;
+
+        public Banco()
+
+        {
+
+            cliente1 = new Cliente("Paco");
+
+            cliente2 = new Cliente("Dora");
+
+            cliente3 = new Cliente("Barney");
+
+        }
+
+        public void Operar()
+
+        {
+
+            cliente1.Depositar(8000);
+
+            cliente2.Depositar(30000);
+
+            cliente3.Depositar(20000);
+
+            cliente3.Extraer(500);
+
+        }
+
+        public void DepositosTotales()
+
+        {
+
+            int t = cliente1.RetornarMonto() +
+
+                    cliente2.RetornarMonto() +
+
+                    cliente3.RetornarMonto();
+
+            Console.WriteLine("El total de dinero en el banco es:" + t);
+
+            cliente1.Imprimir();
+
+            cliente2.Imprimir();
+
+            cliente3.Imprimir();
+
+        }
+
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Nombre:Oscar Carmona Mendez   16-SIIN-1-164  seccion: 0463");
+
+            Console.WriteLine("Nombre:Alfredo Vazquez Diaz   15-MIIN-1-138   seccion: 0463");
+
+            Banco banco1 = new Banco();
+
+            banco1.Operar();
+
+            banco1.DepositosTotales();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
